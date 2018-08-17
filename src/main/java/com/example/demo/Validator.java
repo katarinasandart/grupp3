69ff8fcc5a91db98cb09fbaa8e02981eb36f3e26
katarinasandart <katarina.sandart@hotmail.com>
2018-08-17 15:09:14 +0200

    Add Controller class

--- src/main/java/com/example/demo/Validator.java
+++ src/main/java/com/example/demo/Validator.java
@@ -1,8 +1,9 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Controller
 public class Validator {
 
     @GetMapping("/test")
