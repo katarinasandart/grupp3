a6dc7bd0260f549600edefcb563ce354c445ae73
Mathias Vessberg <mathias.vessberg@gmail.com>
2018-08-17 15:03:51 +0200

    adding interface + index.html

--- src/main/java/com/example/demo/Validator.java
+++ src/main/java/com/example/demo/Validator.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 public class Validator {
+
+    @GetMapping("/test")
     public boolean isNumber(String s) {
         if (s != null && !s.isEmpty()) {
             for (char c : s.toCharArray()) {
