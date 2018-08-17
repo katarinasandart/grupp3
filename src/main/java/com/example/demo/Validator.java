a9b2ddbf320a9b01efea001e92671af5e249e1b5
Administrator <emmaljungdahl@gmail.com>
2018-08-17 16:05:53 +0200

    Email validation method

--- src/main/java/com/example/demo/Validator.java
+++ src/main/java/com/example/demo/Validator.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 
 public class Validator {
@@ -15,5 +16,6 @@ public class Validator {
             }
         }
         return false;
+
     }
 }
